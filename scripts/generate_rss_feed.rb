231ad2f91f03ace2b11a288b263f08e1a277c1f4
anthonysalvato3 <anthonysalvato3@yahoo.com>
2025-01-12 13:56:40 -0600

    More debugging 6

--- scripts/generate_rss_feed.rb
+++ scripts/generate_rss_feed.rb
@@ -42,7 +42,8 @@ def convert_atom_to_rss(atom_file, rss_file, posts_dir, config_file)
             post_file_path = Dir.glob("#{posts_dir}/*.markdown").find do |path|
               begin
                 puts "Processing file: #{path}"
-                front_matter, _ = File.read(path).split(/^---$/, 3)[1, 2]
+                file_content = File.read(path, encoding: 'utf-8')
+                front_matter, _ = file_content.split(/^---$/, 3)[1, 2]
                 puts "Front matter: #{front_matter}"
                 post_metadata = YAML.safe_load(front_matter)
 
