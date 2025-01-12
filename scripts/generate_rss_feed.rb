ba561d4eff5f2c90ae39d5fcd46ea0de8b974651
anthonysalvato3 <anthonysalvato3@yahoo.com>
2025-01-12 14:01:38 -0600

    More debugging 8

--- scripts/generate_rss_feed.rb
+++ scripts/generate_rss_feed.rb
@@ -43,6 +43,7 @@ def convert_atom_to_rss(atom_file, rss_file, posts_dir, config_file)
               begin
                 puts "Processing file: #{path}"
                 file_content = File.read(path, encoding: 'utf-8').scrub
+                puts "File Content for #{path}:\n#{file_content}"
                 front_matter, _ = file_content.split(/^---$/, 3)[1, 2]
                 puts "Front matter: #{front_matter}"
                 post_metadata = YAML.safe_load(front_matter)
